5f0ac3b8cb770b580750121ef62f2fcecc9f99ea
anyone27 <josh@joshcallarman.com>
2022-02-09 10:57:51 +0000

    update react routes to stop blocking email api

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/send_email" />
-              <Route path="*" element={<NotFound />} />
+              {/* <Route path="*" element={<NotFound />} /> */}
             </Routes>
           </div>
 
